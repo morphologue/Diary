02fab767c82ca350a60bf41959674590e2e2c6bb
morphologue <morphologue@users.noreply.github.com>
2018-01-09 21:02:00 +1100

    Restore datepicker icons

--- src/Diary/Scripts/Home/DatePicker.tsx
+++ src/Diary/Scripts/Home/DatePicker.tsx
@@ -1,6 +1,7 @@
 ﻿import * as $ from 'jquery';
 import 'jquery-ui/ui/widgets/datepicker';
 import 'jquery-ui/themes/base/all.css';
+import './jquery-ui-icon-paths.css';
 import * as React from 'react';
 
 interface Props {
